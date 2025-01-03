7460dae0eb2a6883dcfe534d4a250d654416194f
Eric L <eliang58@gmail.com>
2025-01-02 23:54:21 -0500

    disable asterisms for now

--- src/components/celestial-sphere/celestial-sphere.tsx
+++ src/components/celestial-sphere/celestial-sphere.tsx
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { useFrame } from "@react-three/fiber"
 import { useRef } from 'react'
 import { useConstellations, useStarfield } from '../../hooks'
-import asterisms from "../../assets/asterisms.json"
+// import asterisms from "../../assets/asterisms.json"
 import constellations from "../../assets/constellations.json"
 import type { Constellation } from '../../types'
 
@@ -11,28 +11,21 @@ const unitVec = new THREE.Vector3()
 
 export function CelestialSphere() {
   const { mesh: starfieldMesh } = useStarfield()
-  const {
-    mesh: asterismsMesh,
-    labels: asterismsLabels,
-  } = useConstellations(asterisms as Constellation[])
+  // const {
+  //   mesh: asterismsMesh,
+  //   labels: asterismsLabels,
+  // } = useConstellations(asterisms as Constellation[])
   const {
     mesh: constellationsMesh,
     labels: constellationsLabels,
   } = useConstellations(constellations as Constellation[])
-  const asterismsLabelsRef = useRef<THREE.Group>(null)
-  const constellationsLabelsRef = useRef<THREE.Group>(null)
+  const labelsRef = useRef<THREE.Group>(null)
   const needsToRotateRef = useRef(true)
   
   useFrame(() => {
     if (needsToRotateRef.current) {
-      if (asterismsLabelsRef.current) {
-        asterismsLabelsRef.current.children.forEach((child) => {
-          child.lookAt(unitVec)
-        })
-      }
-
-      if (constellationsLabelsRef.current) {
-        constellationsLabelsRef.current.children.forEach((child) => {
+      if (labelsRef.current) {
+        labelsRef.current.children.forEach((child) => {
           child.lookAt(unitVec)
         })
       }
@@ -42,16 +35,16 @@ export function CelestialSphere() {
   return (
     <group>
       <primitive object={starfieldMesh} />
-      <primitive object={asterismsMesh} />
+      {/* <primitive object={asterismsMesh} /> */}
       <primitive object={constellationsMesh} />
       <mesh>
         <sphereGeometry args={[2]} />
         <meshBasicMaterial color={nightColor} side={THREE.BackSide} transparent />
       </mesh>
-      <group ref={asterismsLabelsRef}>
-        {asterismsLabels}
-      </group>
-      <group ref={constellationsLabelsRef}>
+      {/* <group ref={asterismsLabelsRef}>
+      </group> */}
+      <group ref={labelsRef}>
+        {/* {asterismsLabels} */}
         {constellationsLabels}
       </group>
     </group>
